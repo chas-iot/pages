423feaa9ae96b685dcdbef2ed00beb07d0139385
chas-iot <chas-iot@runegate.org>
2020-05-16 00:48:38 +0800

    Revert "maybe..."
    This reverts commit 892a9bc13e2824b40b04126a0e5d7eb4016bb6d6.

--- src/pages-api-handler.js
+++ src/pages-api-handler.js
@@ -30,7 +30,7 @@ class PagesAPIHandler extends APIHandler {
             } else {
                 throw new Error('"db_location" is not in extension configuration');
             }
-            await PagesDB.open(pages_db_location);
+            PagesDB.open(pages_db_location);
         } catch (e) {
             console.error(`pages-api-handler  -  CANNOT CONTINUE  - ${e.message}`);
             throw (e);
