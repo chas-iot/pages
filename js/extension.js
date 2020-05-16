db7ea792c99b0d32994b46af66eaf826c5ee2f2a
chas-iot <chas-iot@runegate.org>
2020-05-17 01:54:55 +0800

    deeper

--- js/extension.js
+++ js/extension.js
@@ -230,7 +230,7 @@
                         if (!item.children || !item.children.length) {
                             console.log('no children', item.nodeName, item.nodeName);
                         } else {
-                            console.log(item.children.length, item.children[item.children.length].nodeName, item.children[item.children.length].nodeName);
+                            console.log(item.children.length, item.children[item.children.length - 1].nodeName, item.children[item.children.length - 1].nodeName);
                             newList.push({ rowid: item.children[3].id.split('/').pop, link_order: index });
                         }
                     });
