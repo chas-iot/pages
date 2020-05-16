4e2813fb846878fa21a610cec8a64b707b6ea2e2
chas-iot <chas-iot@runegate.org>
2020-05-17 01:44:14 +0800

    finding out what's there

--- js/extension.js
+++ js/extension.js
@@ -227,6 +227,7 @@
                     if (!NodeList.prototype.forEach) { NodeList.prototype.forEach = Array.prototype.forEach; }
                     let newList = [];
                     parent.children.forEach((item, index) => {
+                        console.log(item.children.length, item.children[item.children.length].nodeName, item.children[item.children.length].nodeName);
                         newList.push({ rowid: item.children[3].id.split('/').pop, link_order: index });
                     });
                     console.log(JSON.stringify(newList, null, 2));
