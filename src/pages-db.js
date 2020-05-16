2b18ecd560b1cd0e7469d35b892511a5cd3cd73b
chas-iot <chas-iot@runegate.org>
2020-05-16 23:23:08 +0800

    moar syntax errors

--- src/pages-db.js
+++ src/pages-db.js
@@ -19,64 +19,62 @@ const PagesDB = {
 
     get_list: function(rowtype) {
         return this.database.get_all(
-            'SELECT * ' +
-            'FROM principal ' +
-            'WHERE rowtype = ?;', [rowtype]
+            `
+SELECT *
+FROM principal
+WHERE rowtype = ?;`, [rowtype]
         );
     },
 
     get_contents: function(rowid) {
-        return this.database.get_all(
-            'SELECT principal.* ' +
-            ', -1 AS link_rowid ' +
-            ', -1 AS link_order ' +
-            'FROM principal ' +
-            'WHERE rowid = ? ' +
-            'UNION ' +
-            'SELECT principal.* ' +
-            ', link.rowid AS link_rowid ' +
-            ', link.link_order ' +
-            'FROM principal ' +
-            'JOIN link ' +
-            'ON link.contained = principal.rowid ' +
-            'AND link.container = ? ' +
-            'ORDER by link_order;', [rowid, rowid]);
+        return this.database.get_all(`
+SELECT principal.*
+, -1 AS link_rowid
+, -1 AS link_order
+FROM principal
+WHERE rowid = ?
+UNION
+SELECT principal.*
+, link.rowid AS link_rowid
+, link.link_order
+FROM principal
+JOIN link
+ON link.contained = principal.rowid
+AND link.container = ?
+ORDER by link_order;`, [rowid, rowid]);
     },
 
     add_principal: async function(rowtype, name) {
         // there may genuinely be things with duplicate names, 
         // so prevent duplicate groups here rather than with a unique index
-        const t = await this.database.get(
-            'SELECT count(*) AS gc ' +
-            'FROM principal ' +
-            'WHERE name = ? ' +
-            'AND rowtype = ?', [name, rowtype]);
+        const t = await this.database.get(`
+SELECT count(*) AS gc
+FROM principal
+WHERE name = ?
+AND rowtype = ?`, [name, rowtype]);
         if (t.gc > 0) {
             return null;
         }
-        return this.database.run(
-            'INSERT INTO principal (name, rowtype) ' +
-            'VALUES (?, ?);', [name, rowtype]
-        );
+        return this.database.run(`
+INSERT INTO principal (name, rowtype)
+VALUES (?, ?);`, [name, rowtype]);
     },
 
     delete_principal: function(rowid) {
         // due to foreign keys cascading, this should delete the links to the page contents
-        return this.database.run(
-            'DELETE FROM principal ' +
-            'WHERE rowid = ?;', [rowid]
-        );
+        return this.database.run(`
+DELETE FROM principal
+WHERE rowid = ?;`, [rowid]);
 
     },
 
-    insert_link: async function(container, contained, lilink_ordernk_order) {
+    insert_link: async function(container, contained, link_order) {
         if (link_order === null) {
             link_order = 999999;
         }
-        const result = await this.database.run(
-            'INSERT INTO link (container, contained, link_order) ' +
-            'VALUES (?, ?);', [container, contained]
-        );
+        const result = await this.database.run(`
+INSERT INTO link (container, contained, link_order)
+VALUES (?, ?, ?);`, [container, contained, link_order]);
         if (result === null || result.lastid === null) {
             return null;
         }
@@ -84,27 +82,27 @@ const PagesDB = {
             rowid: result.lastID,
             container: container,
             contained: contained,
+            link_order: link_order
         };
     },
 
     delete_link: async function(rowid) {
-        return await this.database.run(
-            'DELETE FROM link ' +
-            'WHERE rowid = ?;', [rowid]);
+        return await this.database.run(`
+DELETE FROM link
+WHERE rowid = ?;`, [rowid]);
     },
 
     get_available_links: async function(rowid, t1, t2, t3) {
         try {
-            return await this.database.get_all(
-                'SELECT * ' +
-                'FROM principal ' +
-                "WHERE rowtype in (?, ?, ?) " +
-                'AND NOT rowid IN ( ' +
-                '  SELECT contained ' +
-                '  FROM link ' +
-                '  WHERE container = ?) ' +
-                'ORDER BY name;', [t1, t2, t3, rowid]
-            );
+            return await this.database.get_all(`
+SELECT *
+FROM principal
+WHERE rowtype in (?, ?, ?)
+AND NOT rowid IN (
+    SELECT contained
+    FROM link
+    WHERE container = ?)
+ORDER BY name;`, [t1, t2, t3, rowid]);
         } catch (e) {
             console.log(e);
             return null;
@@ -112,15 +110,15 @@ const PagesDB = {
     },
 
     upsert_thing: async function(extid, name) {
-        let t = await this.database.get(
-            'SELECT * ' +
-            'FROM principal ' +
-            'WHERE extid = ?;', [extid]);
+        let t = await this.database.get(`
+SELECT *
+FROM principal
+WHERE extid = ?;`, [extid]);
 
         if (t === null || t === undefined || t.extid !== extid) {
-            return this.database.run(
-                'INSERT INTO principal(rowtype,extid, name) ' +
-                'VALUES (?, ?, ?);', ['T', extid, name]);
+            return this.database.run(`
+INSERT INTO principal(rowtype, extid, name)
+VALUES ('T', ?, ?);`, [extid, name]);
         }
 
         if (t.name === name) {
@@ -128,16 +126,17 @@ const PagesDB = {
             return null;
         }
 
-        return this.database.run(
-            'UPDATE principal ' +
-            'SET name = ? ' +
-            'WHERE extid = ?;', [name, extid]);
+        return this.database.run(`
+UPDATE principal
+SET name = ?
+WHERE extid = ?;`, [name, extid]);
     },
 
     cleanup_things: async function(active_things) {
-        return PagesDB.database.run('DELETE FROM principal ' +
-            "WHERE rowtype = 'T' " +
-            `AND NOT extid in (${new Array(active_things.length).fill('?').join(',')})`,
+        return PagesDB.database.run(`
+DELETE FROM principal
+WHERE rowtype = 'T'
+AND NOT extid in (${new Array(active_things.length).fill('?').join(',')})`,
             active_things
         );
     },
@@ -146,22 +145,18 @@ const PagesDB = {
         // Create the groups related tables
         db.serialize(() => {
             db.exec('PRAGMA foreign_keys = ON;') // s/b a no-op if not supported
-                .exec(
-                    'CREATE TABLE IF NOT EXISTS principal (' +
-                    'rowid INTEGER PRIMARY KEY AUTOINCREMENT, ' +
-                    'rowtype TEXT NOT NULL, ' + // values G group; P page; T thing - possible expansion later to U user to support permissions
-                    'name TEXT NOT NULL, ' +
-                    'extid TEXT' +
-                    ');'
-                )
-                .exec(
-                    'CREATE TABLE IF NOT EXISTS link (' +
-                    'rowid INTEGER PRIMARY KEY AUTOINCREMENT,' +
-                    'container INTEGER NOT NULL REFERENCES principal(rowid) ON DELETE CASCADE, ' +
-                    'contained INTEGER NOT NULL REFERENCES principal(rowid) ON DELETE CASCADE ' +
-                    'link_order INTEGER' +
-                    ');'
-                )
+                .exec(`
+CREATE TABLE IF NOT EXISTS principal (
+rowid INTEGER PRIMARY KEY AUTOINCREMENT,
+rowtype TEXT NOT NULL,
+name TEXT NOT NULL,
+extid TEXT );`)
+                .exec(`
+CREATE TABLE IF NOT EXISTS link (
+rowid INTEGER PRIMARY KEY AUTOINCREMENT,
+container INTEGER NOT NULL REFERENCES principal(rowid) ON DELETE CASCADE,
+contained INTEGER NOT NULL REFERENCES principal(rowid) ON DELETE CASCADE,
+link_order INTEGER );`)
                 .exec(
                     'CREATE UNIQUE INDEX IF NOT EXISTS links1 ON link(container, contained);'
                 )
